c55e217c79e54d9b2c9208cf016b57b0d237c668
lizianegarcia <lizianegarciaa@gmail.com>
2024-08-24 19:15:19 -0300

    Including books

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+  };
+  
+  ScrollReveal().reveal(".container .letter-b", {
+    duration: 1000,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".container img", {
+    duration: 1000,
+    delay: 1500,
+  });
+  ScrollReveal().reveal(".container .text__left", {
+    ...scrollRevealOption,
+    origin: "right",
+    delay: 2000,
+  });
+  ScrollReveal().reveal(".container .text__right", {
+    ...scrollRevealOption,
+    origin: "left",
+    delay: 2000,
+  });
+  ScrollReveal().reveal(".container .explore", {
+    duration: 1000,
+    delay: 2500,
+  });
+  ScrollReveal().reveal(".container h5", {
+    duration: 1000,
+    interval: 500,
+    delay: 3000,
+  });
+  ScrollReveal().reveal(".footer p", {
+    duration: 1000,
+    delay: 1000,
+  });
+  ScrollReveal().reveal(".text", {
+    duration: 1200,
+    delay: 1200,
+  });
