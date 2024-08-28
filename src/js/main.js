b00a0ebbeea8475c548a0448b696fbd61cc3d1e2
lizianegarcia <lizianegarciaa@gmail.com>
2024-08-27 22:14:13 -0300

    modal + form validation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
+// ------- ANIMAÇÕES --------
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
   };
-  
+
   ScrollReveal().reveal(".container .letter-b", {
     duration: 1000,
     delay: 1000,
@@ -39,3 +40,79 @@ const scrollRevealOption = {
     duration: 1200,
     delay: 1200,
   });
+
+// ------- MODAL ---------
+  const button = document.querySelector(".contact");
+  const modal = document.querySelector("dialog");
+  const buttonClose = document.querySelector("dialog button");
+  
+button.onclick = function () {
+  modal.showModal();
+};
+
+buttonClose.onclick = function () {
+  modal.close();
+};
+
+
+// ----------- VALIDAÇÃO DE CAMPOS FORM -------------
+const form = document.querySelector(".form");
+const inputs = document.querySelectorAll(".input-required");
+const spans = document.querySelectorAll(".span-required")
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const handlePhone = (e) => {
+  let input = e.target
+  input.value = phoneMask(input.value)
+}
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
+
+function setError (index) {
+  inputs[index].style.border = '2px solid #fc6c6b';
+  spans[index].style.display = 'block';
+};
+
+function removeError (index) {
+  inputs[index].style.border = '';
+  spans[index].style.display = 'none';
+};
+
+function nameValidate () {
+  if(inputs[0].value.length < 3) {
+    setError(0);
+  } else {
+    removeError(0);
+  }
+};
+
+function emailValidate () {
+  if(!emailRegex.test(inputs[1].value)) {
+    setError(1);
+  } else {
+    removeError(1);
+  }
+}
+
+function messageValidate () {
+  if(inputs[2].value.length < 10) {
+    setError(2);
+  } else {
+    removeError(2);
+  }
+};
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  nameValidate();
+  emailValidate();
+  messageValidate();
+  modal.close();
+});
+
+
